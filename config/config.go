c95dcd4c4e0c656cd285380b99c2929ef2757c42
Luke Rohenaz <rohenaz@gmail.com>
2025-02-03 15:38:06 -0500

    fix test

--- config/config.go
+++ config/config.go
@@ -5,8 +5,8 @@ var BitcoinSchemaTypes = []string{"friend", "like", "repost", "post", "message"}
 // There are config constants
 const (
 	SkipSPV           = true
-	MinerAPIEndpoint  = "https://texas1.gorillapool.iom/mapi/tx/"
-	JunglebusEndpoint = "https://junglebus.gorillapool.io/"
+	MinerAPIEndpoint  = "https://mapi.gorillapool.iom/mapi/tx/"
+	JunglebusEndpoint = "https://texas1.junglebus.gorillapool.io/"
 	FromBlock         = 817000                            // "Welcome to the Future" post = 574287
 	BockSyncRetries   = 5                                 // number of retries before block is marked failed
 	DeleteAfterIngest = false                             // delete json data files after ingesting to db. If using p2p this will effective disable seeding (jerk)
